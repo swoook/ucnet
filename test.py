186cc7a35f9d8ae7b53a585219d117929d7ad9d1
swoook <kimseongwoo21@gmail.com>
2021-04-20 14:09:44 +0900

    Fix #5

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import pdb, os, argparse
 from scipy import misc
 from model.ResNet_models import Generator
 from data import test_dataset
+from tqdm import tqdm, trange
 import cv2
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '1'
@@ -14,13 +15,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--testsize', type=int, default=352, help='testing size')
 parser.add_argument('--latent_dim', type=int, default=3, help='latent dim')
 parser.add_argument('--feat_channel', type=int, default=32, help='reduced channel of saliency feat')
+parser.add_argument('--dataset_dir', metavar='DIR', required=True, help='dataset directory for evaluation')
+parser.add_argument('--model_path', metavar='DIR', required=True, help='pre-trained model (.pth) path')
+# parser.add_argument('--depth_dir', metavar='DIR', required=True, help='depth data directory')
 opt = parser.parse_args()
 
-dataset_path = './test_dataset/'
-depth_path = './test_dataset/'
+dataset_path = opt.dataset_dir
+# depth_path = opt.depth_dir
 
 generator = Generator(channel=opt.feat_channel, latent_dim=opt.latent_dim)
-generator.load_state_dict(torch.load('./models/Model_100_gen.pth'))
+generator.load_state_dict(torch.load(opt.model_path))
 
 generator.cuda()
 generator.eval()
@@ -28,21 +32,24 @@ generator.eval()
 test_datasets = ['DES', 'LFSD','NJU2K','NLPR','SIP','STERE']
 #test_datasets = ['STERE']
 
-for dataset in test_datasets:
-    save_path = './results/' + dataset + '/'
+for _, dataset in enumerate(tqdm(test_datasets, desc='1st loop')):
+    save_path = os.path.join(dataset_path, 'results', dataset) # './results/' + dataset + '/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    image_root = dataset_path + dataset + '/RGB/'
-    depth_root = dataset_path + dataset + '/depth/'
+    image_root = os.path.join(dataset_path, dataset, 'RGB') # dataset_path + dataset + '/RGB/'
+    depth_root = os.path.join(dataset_path, dataset, 'depth') # dataset_path + dataset + '/depth/'
     test_loader = test_dataset(image_root, depth_root, opt.testsize)
-    for i in range(test_loader.size):
-        print i
-        image, depth, HH, WW, name = test_loader.load_data()
+    for i in trange(test_loader.size, desc='2nd loop'):
+        # print i
+        image, depth, HH, WW, imgname = test_loader.load_data()
         image = image.cuda()
         depth = depth.cuda()
         generator_pred = generator.forward(image, depth, training=False)
         res = generator_pred
         res = F.upsample(res, size=[WW,HH], mode='bilinear', align_corners=False)
-        res = res.sigmoid().data.cpu().numpy().squeeze()
+        res = res.sigmoid().data.cpu().numpy().squeeze() * 255.0
+        # print(np.min(res))
+        # print(np.max(res))
+        # misc.imsave(os.path.join(save_path, imgname), res)
         cv2.imwrite(os.path.join(save_path, imgname), res)
